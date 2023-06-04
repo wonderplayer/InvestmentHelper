1be297cef395ccc3f0e5f26df0559b087582beb4
Wonderplayer <roland.strakis@protonmail.com>
2023-06-04 18:00:07 +0300

    Create service with writing files

---
+++ index.test.ts
@@ -0,0 +1,3 @@
+import { assertEquals } from "std/testing/assert.ts";
+
+assertEquals(1, 2);
--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-console.log("Herro!");
+const response = await fetch("https://api.github.com/users/denoland");
+const jsonResponse = await response.json();
+const filename = "/home/rolandsstrakis/SoftwareDevelopment/InvestmentHelper/eftResponse.json";
+Deno.writeTextFile(filename, JSON.stringify(jsonResponse));
